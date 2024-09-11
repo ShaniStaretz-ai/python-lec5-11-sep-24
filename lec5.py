a1e366bd1202f8e0ff70dc67d0ff7f6836a464c1
Shani Staretz <shakimi88@gmail.com>
2024-09-11 19:50:12 +0300

    while - usage

---
+++ lec5.py
@@ -0,0 +1,6 @@
+x: int = int(input("please enter the negative number:\n"));
+while x <= 0:
+    print("this number is not positive")
+    x: int = int(input("please enter the negative number:\n"));
+
+print("finish")
