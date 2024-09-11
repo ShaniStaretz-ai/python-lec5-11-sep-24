e5e60b1dce9079f36e24e5ffd4f0e4e3a8e32ecb
Shani Staretz <shakimi88@gmail.com>
2024-09-11 21:14:30 +0300

    while - advanced

--- lec5.py
+++ lec5.py
@@ -1,6 +1,17 @@
-x: int = int(input("please enter the negative number:\n"));
-while x <= 0:
+#ex1
+# x: int = int(input("please enter the positive number:\n"));
+# #x:int =0/-1, giving True condition instead of 1 input() to enter the loop
+# while x <= 0:
+#     print("this number is not positive")
+#     x: int = int(input("please enter the negative number:\n"));
+# print("finish")
+
+x: int = int(input("please enter the positive number:\n"));
+#x:int =0/-1, giving True condition instead of 1 input() to enter the loop
+counter:int=1
+while x <= 0 and counter!=3:
     print("this number is not positive")
     x: int = int(input("please enter the negative number:\n"));
-
+    counter+=1
 print("finish")
+
