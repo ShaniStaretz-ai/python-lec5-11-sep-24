d9fa29c61cf2e67944fca05b429da4dd61e74eb7
Shani Staretz <shakimi88@gmail.com>
2024-09-14 18:07:45 +0300

    homework is ready

---
+++ hw/hw5-2.py
@@ -0,0 +1,29 @@
+# START
+
+# use for loop and print:
+# ex1:
+# all integer numbers from 0 to 10, including
+for i in range(0, 11):
+    print(i, end=" ")
+print()
+
+# ex2:
+#  all even numbers from 40 to 88, including
+for i in range(40, 90, 2):
+    print(i, end=" ")
+print()
+# END
+
+# ex2:
+# all numbers that are divisible by 7 without a remainder
+for i in range(77, 105, 7):
+    print(i, end=" ")
+print()
+# END
+
+# ex2:
+# all numbers that are divisible by 7 without a remainder
+for i in range(99, 66 - 1, -3):
+    print(i, end=" ")
+print()
+# END
