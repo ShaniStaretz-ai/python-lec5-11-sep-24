9e4ac9fcd3bbf6286130c4c2fb1664ca9573a4a7
Shani Staretz <shakimi88@gmail.com>
2024-09-14 18:45:33 +0300

    after CR

--- hw/hw5-1.py
+++ hw/hw5-1.py
@@ -1,4 +1,4 @@
 # START
 movie_length = int(input("please enter the movie's length:"));
-print(f"the movie is {movie_length // 60} hours and {movie_length % 60} minutes");
+print(f"the movie is {movie_length // 60} hours and {movie_length % 60} minutes")
 # END
